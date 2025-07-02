7815cbbb72cda3890507699af46e0d2d908c9df9
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-07-01 22:39:58 -0500

    feat: crear pagina para subir documento

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
+import Upload from "./pages/Upload";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/upload" element={<Upload />} />
       </Routes>
     </>
   );
