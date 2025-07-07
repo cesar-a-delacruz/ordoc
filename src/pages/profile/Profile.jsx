6279b2b4b2873ff338bcb6de214a04f580808e51
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-07-06 21:01:36 -0500

    feat: estilizar el compnente Profile

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import supabase from "../../apis/supabase";
+import "./Profile.css";
 
 function Profile() {
   const [name, setName] = useState("");
@@ -8,53 +9,56 @@ function Profile() {
 
   useEffect(() => {
     (async () => {
-      const user = (await supabase.auth.getUser()).data.user;
-      const profile = (
-        await supabase.from("profiles").select("name, birth").eq("id", user.id)
-      ).data[0];
-
-      setName(profile.name);
-      setBirth(profile.birth);
-      setEmail(user.email);
+      // const user = (await supabase.auth.getUser()).data.user;
+      // const profile = (
+      //   await supabase.from("profiles").select("name, birth").eq("id", user.id)
+      // ).data[0];
+      // setName(profile.name);
+      // setBirth(profile.birth);
+      // setEmail(user.email);
     })();
   }, []);
 
   return (
-    <div>
-      <h2>Datos Personales</h2>
+    <div className="form-container profile">
+      <div className="form-header">
+        <h1>Datos Personales</h1>
+      </div>
       <form>
-        <label htmlFor="name">Nombre Completo:</label>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-        <br />
-        <br />
-        <label htmlFor="birth">Fecha de Nacimiento:</label>
-        <input
-          type="date"
-          id="birth"
-          value={birth}
-          onChange={(e) => setBirth(e.target.value)}
-          required
-        />
-        <br />
-        <br />
-        <label htmlFor="email">E-mail:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <br />
-        <br />
+        <div className="form-group">
+          <label htmlFor="name">Nombre Completo:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="birth">Fecha de Nacimiento:</label>
+          <input
+            type="date"
+            id="birth"
+            value={birth}
+            onChange={(e) => setBirth(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">E-mail:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
 
-        <button type="submit">Registrar</button>
+        <div className="button-group">
+          <button>Actualizar</button>
+        </div>
       </form>
     </div>
   );
