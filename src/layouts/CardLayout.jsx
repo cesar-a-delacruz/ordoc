3b28dedab19bf0d00cc39a12e71133fb44098d7c
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-07-08 03:00:55 -0500

    feat: permitir el cierre de sesión

--- src/layouts/CardLayout.jsx
+++ src/layouts/CardLayout.jsx
@@ -1,20 +1,32 @@
+import supabase from "../apis/supabase";
 import "./CardLayout.css";
 
 function CardLayout({ children }) {
   return (
     <div className="layout">
       <header className="layout header-with-nav">
-          <h1>Ordoc</h1>
-          <nav>
-            <a href="/documents">Documentos</a>
-            <a href="/profile">Perfil</a>
-            <a href="/upload">Subir</a>
-          </nav>
-          <form className="actions">
-            <button type="submit" className="search-button">🔍</button>
-            <input type="text" placeholder="Buscar..." className="search-bar" />
-            <button className="logout-button">Salir</button>
-          </form>
+        <h1>Ordoc</h1>
+        <nav>
+          <a href="/documents">Documentos</a>
+          <a href="/profile">Perfil</a>
+          <a href="/upload">Subir</a>
+        </nav>
+        <form className="actions">
+          <button type="submit" className="search-button">
+            🔍
+          </button>
+          <input type="text" placeholder="Buscar..." className="search-bar" />
+          <button
+            className="logout-button"
+            onClick={async (e) => {
+              e.preventDefault();
+              await supabase.auth.signOut();
+              location.replace("/login");
+            }}
+          >
+            Salir
+          </button>
+        </form>
       </header>
 
       <main className="layout">
