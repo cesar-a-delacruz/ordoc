744107711611f4aaac4f72161645b917fd1b7f8c
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-07-09 17:16:25 -0500

    fix: permitir alteración de campos en Document

--- src/pages/document/Document.jsx
+++ src/pages/document/Document.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import supabase from "../../apis/supabase";
 import CardLayout from "../../layouts/CardLayout";
@@ -7,18 +7,31 @@ import "./Document.css";
 function Documents() {
   const [doc, setDoc] = useState(useLocation().state);
   const [editMode, setEditMode] = useState(false);
+  const [preview, setPreview] = useState(null);
+
+  useEffect(() => {
+    setDoc({ ...doc, name: doc.name.substring(0, doc.name.lastIndexOf(".")) });
+  }, []);
 
   return (
     <CardLayout>
       <div className="document">
         <form>
           <div className="form-group">
-            <img src={doc.url} alt="Preview" className="preview" />
+            <img
+              src={editDoc && preview ? preview : doc.url}
+              alt="Preview"
+              className="preview"
+            />
             {editMode && (
               <input
                 type="file"
                 id="file"
-                onChange={(e) => fillInputs(e)}
+                onChange={(e) => {
+                  const file = e.target.files[0];
+                  const imageURL = URL.createObjectURL(file);
+                  setPreview(imageURL);
+                }}
                 accept=".jpg,.png,"
               />
             )}
@@ -28,13 +41,20 @@ function Documents() {
             <input
               type="text"
               id="name"
-              value={doc.name.substring(0, doc.name.lastIndexOf("."))}
+              value={doc.name}
+              onChange={(e) => setDoc({ ...doc, name: e.target.value })}
               disabled
+              pattern="[a-zA-Z0-9 ]+$"
             />
           </div>
           <div className="form-group">
             <label htmlFor="type">Tipo de documento:</label>
-            <select id="type" value={doc.type} disabled>
+            <select
+              id="type"
+              value={doc.type}
+              onChange={(e) => setDoc({ ...doc, type: e.target.value })}
+              disabled
+            >
               <option value="Certificado">Certificado</option>
               <option value="Licencia">Licencia</option>
               <option value="Identificación">Identificación</option>
@@ -50,6 +70,7 @@ function Documents() {
                 type="date"
                 id="expedition"
                 value={doc.expedition}
+                onChange={(e) => setDoc({ ...doc, expedition: e.target.value })}
                 disabled
               />
             </div>
@@ -61,6 +82,7 @@ function Documents() {
                 type="date"
                 id="expiration"
                 value={doc.expiration}
+                onChange={(e) => setDoc({ ...doc, expiration: e.target.value })}
                 disabled
               />
             </div>
